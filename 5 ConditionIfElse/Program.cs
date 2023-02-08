d0033b2fabe437f6aa0bdfebedd33d5c92bbccc1
Roman Linkov <roman.linkov@vk.com>
2023-02-08 16:38:04 +0300

    Создал программу 5 с ветвлением

---
+++ 5 ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+Console.Write("Введите имя пользователя: ");
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
